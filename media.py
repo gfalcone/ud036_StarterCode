980916f49f8e58d436b7eb9f678e695cbe9d165d
Paolo Genissel <paologenissel@gmail.com>
2018-04-15 13:14:04 +0200

    Added doc for attributes

--- media.py
+++ media.py
@@ -6,6 +6,9 @@ class Movie():
     def __init__(self, title, poster_image_url, trailer):
         """
         Constructor
+        :param title (string): title of the movies
+        :param poster_image_url (string): url of the poster images
+        :param trailer (string): url of the trailer video
         """
         self.title = title
         self.poster_image_url = poster_image_url
